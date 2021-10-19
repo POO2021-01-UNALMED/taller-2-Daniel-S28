994d79aa41f0152abd318b00287aeec24bb316e1
Daniel-S28 <80227692+Daniel-S28@users.noreply.github.com>
2021-10-18 23:32:57 -0500

    Update Asiento.java

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -9,7 +9,7 @@ public class Asiento {
 		if (color=="rojo"||color=="verde"||color=="amarillo"||color=="negro"||color=="blanco") {
 			this.color=color;
 		}
-		else {
+		else if (color!=null) {
 			this.color=null;
 		}
 	}
